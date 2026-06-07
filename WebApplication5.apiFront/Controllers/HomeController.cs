3049efa90b9829401d87a96dc1441ae449e2cdf7
jalya07 <jalavh@code.edu.az>
2026-06-07 21:48:53 +0400

    change

--- WebApplication5.apiFront/Controllers/HomeController.cs
+++ WebApplication5.apiFront/Controllers/HomeController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WebApplication5.apiFront.Models;
 
 namespace WebApplication5.apiFront.Controllers;
 
@@ -19,6 +18,6 @@ public class HomeController : Controller
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        return View();
     }
 }
